f9570e5efb2c30b5aec95c135a5b3de67d0e42d1
bamboo-house <66010352+bamboo-house@users.noreply.github.com>
2024-02-18 19:26:54 +0900

    公式デモのファイルをコピー

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // パスに注意する。src/sample-scenesに置く場合は、staticの引数にsrc/sample-scenesを指定する。
 // npm startをする場所は、webXR-express-testディレクトリですること。
-app.use(express.static("202402081923Desert"));
+app.use(express.static("Build"));
 
 // http
 // app.listen(PORT, () => console.log(`Listening on PORT ${PORT}!`))
